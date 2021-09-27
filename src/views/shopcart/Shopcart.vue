67da1b26424822237e8ccd1de4c903d7cc5f75a2
Nnameir <71417816+Nnameir@users.noreply.github.com>
2021-09-27 23:41:08 +0800

    shopcart update

--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -3,7 +3,9 @@
     <nav-bar class="shopcart-nav-bar">
       <div slot="center">购物车 ({{ purchaseCount }})</div>
     </nav-bar>
-    <shopcart-list />
+    <scroll class="shopcart-content">
+      <shopcart-list />
+    </scroll>
     <shopcart-bottom-bar />
   </div>
 </template>
@@ -13,6 +15,7 @@
 import { mapGetters } from "vuex";
 /* 导入通用组件 */
 import NavBar from "components/common/navbar/NavBar.vue";
+import Scroll from "components/common/scroll/Scroll.vue";
 /* 导入专用组件 */
 import ShopcartList from "./childComponents/ShopcartList.vue";
 import ShopcartBottomBar from "./childComponents/ShopcartBottomBar.vue";
@@ -21,6 +24,7 @@ export default {
   name: "Shopcart",
   components: {
     NavBar,
+    Scroll,
     ShopcartList,
     ShopcartBottomBar,
   },
@@ -35,5 +39,9 @@ export default {
   width: 100%;
   background-color: var(--color-tint);
   color: #fff;
+}
+
+.shopcart-content {
+  height: calc(100vh - 44px - 45px - 49px);
 }
 </style>
